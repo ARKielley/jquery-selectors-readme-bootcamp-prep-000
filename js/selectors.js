ce733f56b9a91f9c821d5540b7d85bceba4e0bd2
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-16 02:04:38 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,15 @@
 function paragraphSelector() {
   return $('p');
 }
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
